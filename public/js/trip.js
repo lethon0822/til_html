3d26a64ae86031f91dd4fe377248e322201a6ff4
1ethon0822 <1ethyjj9240@gmail.com>
2025-05-30 09:31:09 +0900

    trip.js 추가

--- public/js/trip.js
+++ public/js/trip.js
@@ -1,1 +1,12 @@
-window.addEventListener("load", function () {});
+window.addEventListener('load', function () {
+  // api 주소 (파일 주소)
+  const apiUrl = 'http://127.0.0.1:5500/public/api/trip.json';
+  // 현재 출력한 카테고리 (변할 수 있어서 let으로 초기화)
+  let cateNameArr = [];
+  // 리스트 목록 배열
+  let cateListArr = [];
+  // 투어 슬라이드 변수
+  let swtrip;
+
+    
+});
